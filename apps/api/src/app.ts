1c51a52078e4ccd7a615ae85deb3be4aa4501d57
Vishal Pathak <vishalpathak.er@gmail.com>
2026-05-18 14:33:04 +0530

    feat(logger): integrate logger package and update server logging

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,10 +1,10 @@
 import express from "express";
 import type { Application } from "express";
 import { errorHandler } from "@/utils/errorHandler.js";
+import {} from "@archiq/logger"
 
 export function CreateApp(): Application {
   const app = express();
-
-  app.use(errorHandler)
+  app.use(errorHandler);
   return app;
 }
--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,22 @@
 import { env } from "@/config/env.js";
 import { CreateApp } from "./app.js";
+import { createAppLogger } from "@archiq/logger";
 
 async function StartServer() {
   const app = CreateApp();
+  const log = createAppLogger({
+    service: "api",
+    production: env.NODE_ENV === "production",
+  });
+
   const server = app.listen(env.PORT, () => {
-    console.log(`Server running on port : ${env.PORT}`);
+    log.info(
+      {
+        port: env.PORT,
+        url: `http://localhost:${env.PORT}`,
+      },
+      `Server Started`,
+    );
   });
 
   process.on("SIGTERM", () => {
