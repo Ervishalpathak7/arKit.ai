85807bde69a681066234ffffb91be56185d4a6cf
Vishal Pathak <vishalpathak.er@gmail.com>
2026-05-19 10:58:34 +0530

    feat(metrics): add Prometheus integration with metrics endpoint and configuration

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,10 +1,21 @@
 import express from "express";
 import type { Application } from "express";
 import { errorHandler } from "@/utils/errorHandler.js";
-import {} from "@archiq/logger"
+import {} from "@archiq/logger";
+import { register } from "@/observability/metrics.js";
 
 export function CreateApp(): Application {
   const app = express();
+
+ app.get("/metrics", async (_, res) => {
+  res.setHeader(
+    "Content-Type",
+    register.contentType,
+  );
+  const metrics = await register.metrics();
+  res.send(metrics);
+});
+
   app.use(errorHandler);
   return app;
 }
