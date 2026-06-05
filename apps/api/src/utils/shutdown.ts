1daaf0ba2dcbc2e03f43ad152967608df3518f14
Vishal Pathak <vishalpathak.er@gmail.com>
2026-06-05 20:39:00 +0530

    feat: implement graceful shutdown functionality with database and cache disconnection

--- apps/api/src/utils/shutdown.ts
+++ apps/api/src/utils/shutdown.ts
@@ -1,8 +1,14 @@
 import { log } from "@/config/logger.js";
+import { dissconnectCache } from "@archiq/cache";
+import { dissconnectDb } from "@archiq/db";
+import { dissconnectQueue } from "@archiq/queue";
 import { Server } from "http";
 
 export const gracefulShutdown = async (signal: string, server: Server) => {
   log.info(`[${signal}] Shutdown initiated`);
+  await dissconnectDb();
+  await dissconnectCache();
+  await dissconnectQueue();
   server.close(() => {
     log.info(`Server Closed`);
     process.exitCode = 0;
