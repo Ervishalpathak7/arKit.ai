f7af3d232c19fa19a78122e776386eafeb2c3d11
vishal pathak <vishalpathak.er@gmail.com>
2026-08-24 16:03:56 +0530

    refactor: rename package from com.arkit to com.arckit and restore MainApplication and tests

--- src/main/java/com/arckit/main/MainApplication.java
+++ src/main/java/com/arckit/main/MainApplication.java
@@ -1,4 +1,4 @@
-package com.arkit.main;
+package com.arckit.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/arckit/main/MainApplicationTests.java
+++ src/test/java/com/arckit/main/MainApplicationTests.java
@@ -1,4 +1,4 @@
-package com.arkit.main;
+package com.arckit.main;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
